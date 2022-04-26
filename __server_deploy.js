929e47ff7354ba95a1b5563b4a842b865349c5ed
DevMohi <morphex72@gmail.com>
2022-04-27 02:24:16 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/*
+1.ONE TIME************
+*Create Heroku account
+2.verify email
+3.install heroku cli
+4.heroku login in browser
+********************
+*******************
+2.For each project
+********************
+1.heroku create
+
+
+*/
